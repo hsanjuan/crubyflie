52e53ff24df150b933c83a886cdc909986f29a5b
Hector Sanjuan <hector@convivencial.org>
2015-06-21 00:23:48 +0200

    x10 the number of retries before disconnect, as my dongle disconnects way too often compared to the official client

--- lib/crubyflie/driver/radio_driver.rb
+++ lib/crubyflie/driver/radio_driver.rb
@@ -67,7 +67,7 @@ module Crubyflie
         # Default size for the outgoing queue
         OUT_QUEUE_MAX_SIZE = 50
         # Default number of retries before disconnecting
-        RETRIES_BEFORE_DISCONNECT = 10
+        RETRIES_BEFORE_DISCONNECT = 100
 
         attr_reader :uri
         attr_reader :retries_before_disconnect, :out_queue_max_size
